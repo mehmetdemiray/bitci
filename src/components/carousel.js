a3644f13abc0c0eebceda15680ca6cc5e9bfa5b2
Mehmet Demiray <mehmetdemiray@outlook.com.tr>
2020-07-08 02:02:24 +0300

    Carousel Ok

---
+++ src/components/carousel.js
@@ -0,0 +1,44 @@
+﻿import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Card} from './cards';
+
+export const Carousel = (props) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [count, setCount] = useState(0);
+    const [inPage, setInPage] = useState(4);
+    const [activePage, setActivePage] = useState(0);
+    const [margin, setMargin] = useState(50);
+    const [marginH, setMarginH] = useState(0)
+    const [maxCardWidth, setMaxCardWidth] = useState();
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios('https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&q=beautiful+landscape&image_type=photo',);
+        setData(result.data.hits);
+        setCount(result.data.hits.length);
+        setLoading(false);
+      };
+
+      fetchData();
+    }, []);
+
+    useEffect(() => {
+      setMaxCardWidth((props.maxWidth-((inPage-1)*margin))/inPage)
+    }, [props.maxWidth, inPage, margin])
+
+    useEffect(() => {
+      setActivePage(props.direction)
+      setMarginH(props.direction*(maxCardWidth + margin))
+    }, [props.direction])
+
+
+    return (
+      loading ? null:
+        <div className="carousel" style={{height: `${maxCardWidth}px`, marginLeft: `${marginH}px`, width: `${(count*(maxCardWidth + margin))-margin}px`}}>
+            {data.map((item) =>
+              <Card key={item.id} data={item} margin={margin} cardWidth={maxCardWidth} />
+            )}
+        </div>
+    )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,24 @@
-﻿import React from 'react';
+﻿import React, {useEffect, useRef, useState} from 'react';
+import {useWindowWidth} from '@react-hook/window-size/throttled'
+import {Carousel} from './carousel';
 
 export const Layout = () => {
+    const ref = useRef(null);
+    const onlyWidth = useWindowWidth()
+    const [maxWidth, setMaxWidth] = useState()
+    const [direction, setDirection] = useState(0)
+
+    useEffect(() => {
+        setMaxWidth(ref.current ? ref.current.offsetWidth : 0);
+    }, [ref, onlyWidth]);
+
     return (
-        <div className="outer">
+        <div ref={ref} className="outer">
             <header><h1>Carousel Example</h1></header>
-            <div className="content"></div>
+            <Carousel maxWidth={maxWidth} direction={direction}/>
             <footer>
-                <button>Prev</button>
-                <button>Next</button>
+                <button  onClick={() => setDirection(direction + 1)}>Prev</button>
+                <button onClick={() => setDirection(direction - 1)}>Next</button>
             </footer>
         </div>
     )
