20cdab79db9698f35d3d00e5a3828ef7bbc231e4
Mehmet Demiray <mehmetdemiray@outlook.com.tr>
2020-07-11 18:42:49 +0300

    IE Fix!!

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,7 +3,6 @@ import {useSelector} from 'react-redux';
 
 export const Card = (props) => {
     const settings = useSelector(state => state.SETTINGS)
-    console.log(props)
     return (
         <div className="card-outer" style={{marginRight: `${props.margin}px`,}}>
             <img alt={props.data.id} src={props.data.webformatURL} height={`${props.cardWidth}px`} width={`${props.cardWidth}px`}></img>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import "@babel/polyfill";
+
 import React from 'react';
 import './dist/scss/styles.scss';
 import App from './App';
